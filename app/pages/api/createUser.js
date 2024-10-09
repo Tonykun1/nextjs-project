42e989c61c0b8c3dd0d93542fa7ea629e8e9dba1
TonyKun <tal96047@gmail.com>
2024-10-09 13:43:09 +0300

    start work with nextjs

--- app/pages/api/createUser.js
+++ app/pages/api/createUser.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
 
-const filePath = path.join(process.cwd(), 'data', 'users.json'); 
+const filePath = "../../data/users.json"; 
 
 export default function POST(req, res) {
   if (req.method === 'POST') {
